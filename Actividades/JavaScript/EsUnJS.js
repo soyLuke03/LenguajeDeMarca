bf8c7350983186fa75f600a51b0f94ef2b351067
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-03-24 10:08:27 +0100

    Add files via upload

---
+++ Actividades/JavaScript/EsUnJS.js
@@ -0,0 +1,90 @@
+
+//Ejercicios basicos de String y Math
+
+// Ejerc 1
+    /*
+
+let numero = -4;
+let palabra = 'Hola mundo';
+let respuesta = true;
+const pi = 3.14;
+numero = 54;
+// se declara undefined
+console.log(numero,palabra,respuesta,pi);
+//pi = 3.1415; da error porque es una constante
+
+    */
+
+// Ejerc 2
+    /*
+let n1 = 5;
+let n2 = 2;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
+console.log(n1%n2);
+
+let miNombre = 'Luke';
+console.log('hola ' + miNombre);
+    */
+
+// Ejerc 3
+    /*
+let a = 5
+console.log(a);
+console.log(a++);
+console.log(a);
+// Se muestra 'a' y 'a' tras sumarle 1.
+// Se mostraría un 6 aunque 'a' pasa a ser 7.
+console.log(a);
+console.log(a--);
+console.log(a);
+console.log(a-3);
+    */
+
+// Ejerc 4
+    /*
+let cadena = 'Hola mundo';
+console.log(cadena.length);
+console.log(cadena.toUpperCase);
+console.log(cadena.toLowerCase);
+console.log(cadena.indexOf('o'));
+console.log(cadena.indexOf('Hola'));
+console.log(cadena.replace('mundo','Youtube'));
+console.log(cadena.substring(5));
+console.log(cadena.startsWith('h'));
+console.log(cadena.startsWith('H'));
+console.log('r'.repeat(10));
+let nombre='luke';let apellido = 'gonzalez';let edad = 19;
+console.log(`${nombre} ${apellido} ${edad}`);
+    */
+
+// Ejerc 5
+    /*
+let numeroRandom = Math.round(Math.random()*100); console.log(numeroRandom);
+console.log(Math.PI);
+console.log(Math.floor(Math.random() * (10 - 5)) + 5);
+console.log(Math.sign(-5)); // Muestra si es positivo o negativo
+console.log(Math.sign(0));
+console.log(Math.sign(5));
+    */
+
+//Ejercicios basicos de Condicionales
+
+//Ejercicios basicos de Operador Ternario
+
+//Ejercicios basicos de Arrays y Metodos
+
+//Ejercicios basicos de Bucles
+
+//Ejercicios basicos Orientacion a Objetos
+
+//Ejercicios basicos Funciones
+
+//Ejercicios Clases
+
+//Ejercicios Arrays
+
+//Ejercicios DOM
